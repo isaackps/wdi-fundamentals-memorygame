3f9eca71581cc00a900cb28503c016be4cc169bd
Isaac Koh <isaackoh@Isaacs-MacBook-Pro.local>
2017-05-12 23:28:54 +0800

    created game Logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 //console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["king", "king", "queen", "queen"];
+var cardsInPlay = [];
+var cardOne;
+var cardTwo;
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+cardOne = cards[0];
+cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("youfound a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+
+
+console.log(cardOne);
+console.log(cardsInPlay);
+//console.log("User flipped " + cardTwo);
