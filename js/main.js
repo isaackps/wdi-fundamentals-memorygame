15c3987889ca7ced18018156d69ff622c592770b
Isaac Koh <isaackoh@Isaacs-MacBook-Pro.local>
2017-05-13 08:19:12 +0800

    done functions

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
-//console.log("Up and running!");
-
+//create an array to store the value of the cards
 var cards = ["king", "king", "queen", "queen"];
+//create an array to hole the selected cards when the user selects it.
 var cardsInPlay = [];
-var cardOne;
-var cardTwo;
 
-cardOne = cards[0];
-cardTwo = cards[2];
+//create a function to check for a match
+var checkForMatch = function() {
+		//to check if the selected cards are a match
+		if(cardsInPlay[0] === cardsInPlay[1]) {
+			alert("you found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+};
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+//create a function to push the cards selected into the cardsInPlay[] array.
+var flipCard = function(cardsId) {
+	console.log("User flipped" + " " + cards[cardsId]);
+	cardsInPlay.push(cards[cardsId]);
 
-if(cardsInPlay.length === 2) {
-	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("youfound a match!");
-	} else {
-		alert("Sorry, try again.");
+	//to check the number of cards being selected
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
 	}
-}
+};
+
+//call the function
+flipCard(0);
+flipCard(2);
 
 
-
-console.log(cardOne);
-console.log(cardsInPlay);
 //console.log("User flipped " + cardTwo);
