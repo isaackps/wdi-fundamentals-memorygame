2b508d39b136fb9c993fdda3d0fa9341c9079f31
Isaac Koh <isaackoh@Isaacs-MacBook-Pro.local>
2017-05-14 23:26:32 +0800

    game working

--- js/main.js
+++ js/main.js
@@ -34,25 +34,62 @@ var checkForMatch = function() {
 		}
 };
 
+
 //create a function to push the cards selected into the cardsInPlay[] array.
-//we only want to see the rank of the cards.
-var flipCard = function(cardsId) {
-	console.log("User flipped" + " " + cards[cardsId].rank);
-	cardsInPlay.push(cards[cardsId].rank);
-
+var flipCard = function() {
+	//console.log("User flipped" + " " + cards[].rank);
+	//cardsInPlay.push(cards[].rank);
+	var cardId = this.getAttribute('data-id');
 	//show cardimage and suit
-	console.log(cards[cardsId].cardImage);
-	console.log(cards[cardsId].suit);
-
+	//console.log(cards[cardsId].cardImage);
+	//console.log(cards[cardsId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	//to check the number of cards being selected
 	if(cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 };
 
-//call the function
-flipCard(0);
-flipCard(2);
+var cardElement;
+//createBoard for the game
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+		}
+};
+/*
+var resetButton = function() {
+	var resetBtn = document.createElement('button');
+	resetBtn.setAttribute('text-align', 'center');
+	resetBtn.setAttribute('margin', '20px');
+	resetBtn.textContent = 'reset';
+	document.getElementById('reset').appendChild(resetBtn);
+	resetBtn.addEventListener('click', resetGame);
+	//resetBtn.setAttribute('data-id', 'null');
+};
 
+var resetGame = function() {
+		this.setAttribute('src', 'images/back.png');
+	while (cardsInPlay.length > 0) {
+		cardsInPlay.pop();
+	}	
+	//cardElement.setAttribute('data-id', null);
+	//var cardBack = this.getElementsByTagName('img');
+	//cardBack.setAttribute('src', 'images/back.png');
+
+};
+*/
+
+//call the function
+createBoard(); 
+//resetButton();
+
+//flipCard(0);
+//flipCard(2);
 
 //console.log("User flipped " + cardTwo);
